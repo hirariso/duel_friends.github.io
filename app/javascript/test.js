71325a88599465097c0b5d9d79a4900718c7bc3c
Wataru Hirano <Isindensin@softbank.ne.jp>
2020-09-26 20:04:40 +0900

    add animation at top_page

--- app/javascript/test.js
+++ app/javascript/test.js
@@ -1,14 +1,14 @@
-$(document).on('turbolinks:load', function() {
-    $('.menu-icon').click(function(){
+$(document).on('turbolinks:load', function () {
+    $('.home_image').fadeIn('slow');
+    $('.menu-icon').click(function () {
         $('.menu-page').slideToggle();
     });
-    $('.post_image').click(function(){
+    $('.post_image').click(function () {
         var src = $(this).attr('src');
-        $('.big_image').attr('src',src);
+        $('.big_image').attr('src', src);
         $('.big_post_image').fadeIn();
     });
-    $('.close-modal').click(function(){
+    $('.close-modal').click(function () {
         $('.big_post_image').fadeOut();
     });
-   
-  });
+});
